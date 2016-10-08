54677ac42e728b4c4e716f7c46da8a09765aad14
Evan Dixon <evanldixon@live.com>
2016-10-08 14:30:22 -0500

    Made toolkit functional

--- ToolkitConsole/Module1.vb
+++ ToolkitConsole/Module1.vb
@@ -1,42 +1,102 @@
-﻿Module Module1
+﻿Imports System.IO
+Imports System.Reflection
+
+Module Module1
 
     Sub Main()
         Try
-            Using c As New DotNet3dsToolkit.Converter
-                Console.WriteLine("Extracting...")
-                Dim options As New DotNet3dsToolkit.ExtractionOptions
-                options.SourceRom = IO.Path.Combine(Environment.CurrentDirectory, "rom.3ds")
-                options.DestinationDirectory = IO.Path.Combine(Environment.CurrentDirectory, "RawFiles")
+            Console.WriteLine(".Net 3DS Toolkit v{0}", Assembly.GetExecutingAssembly.GetName.Version.ToString(3))
 
-                c.Extract(options).Wait()
+            Dim args = Environment.GetCommandLineArgs
+            If args.Length < 3 Then
+                Console.WriteLine("Usage: ToolkitConsole.exe <source> <destination> [-key0]")
+                Console.WriteLine("<source> can either be a decrypted CCI/3DS ROM, or a directory created by ToolkitConsole.exe")
+                Console.WriteLine("<destination> can be a *.3DS, *.3DZ, *.CCI, or *.CIA file, or a directory if the source is a ROM.")
+                Console.WriteLine("Output format is detected by the extension.  *.CIA files are outputted as CIA files, *.3DZ files are outputted as 0-key encrypted CCI ROMs, all others are outputted as decrypted CCI ROMs.  Use the -key0 flag to output as a 0-key encrypted CCI ROM instead.")
+            Else
+                Dim key0 As Boolean = args.Contains("key0")
+                Dim source As String = args(1)
+                Dim destination As String = args(2)
 
-                'Console.WriteLine("Building decrypted...")
-                'Dim options2 As New DotNet3dsToolkit.BuildOptions
-                'options2.SourceDirectory = IO.Path.Combine(Environment.CurrentDirectory, "RawFiles")
-                'options2.DestinationROM = IO.Path.Combine(Environment.CurrentDirectory, "PatchedRom-Test-Decrypted.3ds")
-                'options2.CompressCodeBin = False
+                If File.Exists(source) Then
 
-                'c.Build3DSDecrypted(options2).Wait()
+                    'Extraction mode
+                    Using c As New DotNet3dsToolkit.Converter
+                        Dim options As New DotNet3dsToolkit.ExtractionOptions
+                        options.SourceRom = source
+                        options.DestinationDirectory = destination
 
-                Console.WriteLine("Building 0-key...")
-                Dim options3 As New DotNet3dsToolkit.BuildOptions
-                options3.SourceDirectory = IO.Path.Combine(Environment.CurrentDirectory, "RawFiles")
-                options3.DestinationROM = IO.Path.Combine(Environment.CurrentDirectory, "PatchedRom-Test-Gateway.3ds")
-                options3.CompressCodeBin = False
+                        Console.WriteLine("Extracting to ""{0}""...", destination)
 
-                c.Build3DS0Key(options3).Wait()
+                        c.Extract(options).Wait()
 
-                Console.WriteLine("Building cia...")
-                Dim options4 As New DotNet3dsToolkit.BuildOptions
-                options4.SourceDirectory = IO.Path.Combine(Environment.CurrentDirectory, "RawFiles")
-                options4.DestinationROM = IO.Path.Combine(Environment.CurrentDirectory, "PatchedRom-Test-Decrypted.cia")
-                options4.CompressCodeBin = False
+                        Console.WriteLine("Extraction complete!")
+                    End Using
 
-                c.BuildCia(options4).Wait()
-            End Using
+                ElseIf Directory.Exists(source) Then
+                    'Building mode
+
+                    If key0 Then
+                        BuildKey0(source, destination)
+                    ElseIf Path.GetExtension(destination).ToLower = "cia" Then
+                        BuildCIA(source, destination)
+                    ElseIf Path.GetExtension(destination).ToLower = "3dz" Then
+                        BuildKey0(source, destination)
+                    Else
+                        BuildDecryptedCCI(source, destination)
+                    End If
+
+                Else
+                    Console.WriteLine("Error: The given source is neither a file nor a directory.")
+                    Console.WriteLine("Source: ""{0}""", source)
+                End If
+
+            End If
         Catch ex As Exception
-            Throw
+            Console.WriteLine(ex.ToString)
         End Try
+    End Sub
+
+    Private Sub BuildKey0(source As String, destination As String)
+        Using c As New DotNet3dsToolkit.Converter
+            Dim options As New DotNet3dsToolkit.BuildOptions
+            options.SourceDirectory = source
+            options.DestinationROM = destination
+
+            Console.WriteLine("Building as key-0 encrypted CCI to ""{0}""...", destination)
+
+            c.Build3DS0Key(options).Wait()
+
+            Console.WriteLine("Build complete!")
+        End Using
+    End Sub
+
+    Private Sub BuildCIA(source As String, destination As String)
+        Using c As New DotNet3dsToolkit.Converter
+            Dim options As New DotNet3dsToolkit.BuildOptions
+            options.SourceDirectory = source
+            options.DestinationROM = destination
+
+            Console.WriteLine("Building as CIA to ""{0}""...", destination)
+
+            c.BuildCia(options).Wait()
+
+            Console.WriteLine("Build complete!")
+        End Using
+    End Sub
+
+    Private Sub BuildDecryptedCCI(source As String, destination As String)
+        Using c As New DotNet3dsToolkit.Converter
+            Dim options As New DotNet3dsToolkit.BuildOptions
+            options.SourceDirectory = source
+            options.DestinationROM = destination
+
+            Console.WriteLine("Building as decrypted CCI to ""{0}""...", destination)
+
+            c.Build3DSDecrypted(options).Wait()
+
+            Console.WriteLine("Build complete!")
+        End Using
     End Sub
 
 End Module
