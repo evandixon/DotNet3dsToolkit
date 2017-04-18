2284ef366545c295cff2777c9d234c4b29ff1c62
Evan Dixon <evanldixon@live.com>
2017-04-18 16:44:24 -0500

    Added .Net Core console app for .Net Standard Library

---
+++ ToolkitConsoleCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using DotNet3dsToolkit.Core;
+using SkyEditor.Core.IO;
+using System;
+
+namespace ToolkitConsoleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = args[0];
+            var dir = args[1];
+            using (var file = new NdsRom())
+            {
+                file.OpenFile(filename, new PhysicalIOProvider()).Wait();
+                file.Unpack(dir, new PhysicalIOProvider()).Wait();
+            }
+        }
+    }
+}
